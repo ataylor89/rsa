3d46e9e51363ecbf166780b24005ea9015dfa75c
Andrew Taylor <rolyata47@gmail.com>
2025-07-19 08:54:25 -0500

    Simplifying keygen.py

--- keygen.py
+++ keygen.py
@@ -2,18 +2,18 @@ import keytable
 import random
 import sys
 
-def create_keys(keylen=4):
+def create_key_pair(keylen=4):
     if not valid(keylen):
         return None
+
     nvalues = list(keytable.table.keys())
     nvalues = random.sample(nvalues, keylen)
+
     keys = []
     for nvalue in nvalues:
         (n, p, q, phi, e, d) = keytable.table[nvalue]
         keys.append((n, e, d))
-    return keys
 
-def save(keys):
     with open("publickey.txt", "w") as file:
         for (n, e, d) in keys:
             file.write("n=%d e=%d\n" %(n, e))
@@ -23,12 +23,15 @@ def save(keys):
             file.write("n=%d d=%d\n" %(n, d))
 
 def valid(keylen):
-    num_nvalues = len(keytable.table.keys())
-    return keylen <= num_nvalues
+    return keylen <= len(keytable.table)
 
 def main():
     if len(sys.argv) > 2:
         print("Usage: python keygen.py")
+        sys.exit(0)
+
+    if not keytable.load("keytable.pickle"):
+        print("The key table does not exist")
         sys.exit(0)
 
     if len(sys.argv) == 2:
@@ -36,16 +39,12 @@ def main():
     else:
         keylen = 4
 
-    if not keytable.load("keytable.pickle"):
-        print("The key table does not exist")
-        sys.exit(0)
-
     if not valid(keylen):
         print("The key length exceeds the number of entries in the key table")
         sys.exit(0)
 
-    keys = create_keys(keylen)
-    save(keys)
+    create_key_pair(keylen)
+    print("Created key pair in publickey.txt and privatekey.txt")
     
 if __name__ == "__main__":
     main()
