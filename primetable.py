7360308ca025578459ffc7f4267fcdb117fb0eef
Andrew Taylor <rolyata47@gmail.com>
2025-08-01 08:53:15 -0500

    Simplifying code

--- primetable.py
+++ primetable.py
@@ -3,45 +3,19 @@ import sys
 import os
 import time
 
-flags = {'existing_table_sufficient': False}
-
-def get(n):
-    return table[n]
-
-def size():
-    return len(table)
-
-def load(path='primetable.pickle'):
-    global table
-    if os.path.exists(path):
-        file = open(path, "rb")
-        table = pickle.load(file)
-        return True
-    return False
-
-def save(path='primetable.pickle'):
-    global table
-    if table and not flags['existing_table_sufficient']:
-        file = open(path, "wb")
-        pickle.dump(table, file)
-        return True
-    return False
+table = []
 
 def generate(n):
-    global table
-
-    if load('primetable.pickle') and len(table) >= n:
-        flags['existing_table_sufficient'] = True
-        return
-
+    if len(table) >= n:
+        return False
     size = 10 * n
     s = sieve(n, size)
     while s.count('P') < n:
         size *= 10
         s = sieve(n, size)
-    
-    table = [i for i, j in enumerate(s) if j == 'P'][0:n]
-    flags['existing_table_sufficient'] = False
+    table.clear()
+    table.extend([i for i, j in enumerate(s) if j == 'P'][0:n])
+    return True
 
 def sieve(n, size):
     arr = ['P'] * size
@@ -55,31 +29,42 @@ def sieve(n, size):
                 j += 1
     return arr
 
+def get(n):
+    return table[n]
+
+def size():
+    return len(table)
+
+def load(path='primetable.pickle'):
+    if os.path.exists(path):
+        with open(path, "rb") as file:
+            table.clear()
+            table.extend(pickle.load(file))
+
+def save(path='primetable.pickle'):
+    if len(table) > 0:
+        with open(path, "wb") as file:
+            pickle.dump(table, file)
+
 def main():
-    start_time = time.time()
-    error_msg = "Usage: python primetable.py <numberofprimes>"
-
     if len(sys.argv) != 2:
-        print(error_msg)
-        sys.exit(0)
-
+        print("Usage: python primetable.py <numberofprimes>")
+        return
     try:
         n = int(sys.argv[1])
+        assert n > 0
     except:
-        print(error_msg)
-        sys.exit(0)
-
-    if n <= 0:
-        print(error_msg)
-        sys.exit(0)
- 
-    generate(n)
-
-    if flags["existing_table_sufficient"]:
-        print("Existing table is sufficient")
+        print("Unable to parse n as a positive integer")
+        return
+    load()
+    st = time.time()
+    modified = generate(n)
+    te = time.time() - st
+    if modified:
+        save()
+        print("Created a prime table with %d primes in %s seconds" %(n, te))
     else:
-        save("primetable.pickle")
-        print("Created a prime table with %d primes in %s seconds" %(n, time.time() - start_time))
+        print("The existing table is sufficient.")
 
 if __name__ == "__main__":
     main()
