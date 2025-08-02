3b0c3f1effdae63d5d90a87b6b2f2838f438883f
Andrew Taylor <rolyata47@gmail.com>
2025-08-02 16:06:09 -0500

    Printing to stdout instead of writing to file

--- encrypt.py
+++ encrypt.py
@@ -18,13 +18,11 @@ def main():
     if len(sys.argv) != 2: 
         print("Usage: python encrypt.py <messagefile>")
         sys.exit(0)
-    msgfile = open(sys.argv[1], "r")
-    msg = msgfile.read()
+    file = open(sys.argv[1], "r")
+    msg = file.read()
     key = parser.parse_key("publickey.txt")
     ciphertext = encrypt(msg, key)
-    with open("cipher.txt", "wb") as cipherfile:
-        binarydata = ciphertext.encode("utf-8")
-        cipherfile.write(binarydata)
+    print(ciphertext, end='')
 
 if __name__ == "__main__":
     main()
