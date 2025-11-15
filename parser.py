abc306a9ab8c7d9787c64f388832eddc7d435188
Andrew Taylor <rolyata47@gmail.com>
2025-11-15 13:28:24 -0600

    Changing double quotes to single quotes

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 def parse_key(path):
     key = []
-    file = open(path, "r")
+    file = open(path, 'r')
     for line in file:
         tokens = line.split()
         n, e, d = int(tokens[0][2:]), int(tokens[1][2:]), int(tokens[2][2:])
