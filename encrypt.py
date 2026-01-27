bff6610aa014658d8c33bc277ca9e1ef8374bc4c
Andrew Taylor <rolyata47@gmail.com>
2026-01-27 17:50:53 -0600

    Fixing a problem; it should now work (hopefully) for characters like π

--- encrypt.py
+++ encrypt.py
@@ -7,7 +7,7 @@ import sys
 
 def encrypt(msg, key):
     ciphertext = ''
-    codes = msg.encode('utf-8')
+    codes = list(map(lambda x: ord(x), msg))
     keylen = len(key)
     for i in range(0, len(codes)):
         (n, e, d) = key[i % keylen]
