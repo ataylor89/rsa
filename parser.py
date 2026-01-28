74ee1ed27ac465e3f683ba89085ed7977c7aea24
Andrew Taylor <rolyata47@gmail.com>
2026-01-27 21:02:30 -0600

    Adding error handling to parser.py

--- parser.py
+++ parser.py
@@ -1,8 +1,16 @@
+from exceptions import InvalidKeyError
+
 def parse_key(path):
     key = []
-    file = open(path, 'r')
-    for line in file:
-        tokens = line.split()
-        n, e, d = int(tokens[0][2:]), int(tokens[1][2:]), int(tokens[2][2:])
-        key.append((n, e, d))
+    with open(path, 'r') as file:
+        for index, line in enumerate(file):
+            linenum = index + 1
+            try:
+                tokens = line.split()
+                n, e, d = int(tokens[0][2:]), int(tokens[1][2:]), int(tokens[2][2:])
+                key.append((n, e, d))
+            except ValueError as err:
+                raise InvalidKeyError(f'Value could not be parsed as an integer in line {linenum} of key file')
+            except IndexError as err:
+                raise InvalidKeyError(f'Value missing in line {linenum} of key file')
     return key
